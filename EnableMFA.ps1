042740ebad6873c6ea60244190e10ecbe674e03e
ScubaAndy <51924186+ScubaAndy@users.noreply.github.com>
2023-05-19 15:10:52 -0700

    update

---
+++ EnableMFA.ps1
@@ -0,0 +1,21 @@
+﻿#Connect-MsolService
+  
+    $st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+ 
+    $st.RelyingParty = "*"
+ 
+    $st.State = "Enabled"
+ 
+    $sta = @($st)
+ 
+
+
+
+
+
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
+Set-MsolUser -UserPrincipalName user@company.com  -StrongAuthenticationRequirements $sta
