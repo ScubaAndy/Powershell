28ced582bbe2414423e9773ca10da099142cbbce
ScubaAndy <51924186+ScubaAndy@users.noreply.github.com>
2023-05-19 15:19:33 -0700

    new

---
+++ InstalledModule.ps1
@@ -0,0 +1,9 @@
+Get-InstalledModule
+
+Install-Module -Name ExchangeOnlineManagement
+Install-Module -Name MicrosoftTeams
+Install-Module -Name MSOnline
+Install-Module -Name ReportingServicesTools
+Install-Module -Name Azure
+Install-Module -Name AzureAD
+Install-Module -Name ReportingServicesTools
