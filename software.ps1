e6342752d51c20ba501d946129504adbde8a4629
ScubaAndy <51924186+ScubaAndy@users.noreply.github.com>
2023-05-19 09:29:10 -0700

    Update software.ps1
    updated

--- software.ps1
+++ software.ps1
@@ -1,1 +1,50 @@
-test
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+winget install -e --id rcmaehl.MSEdgeRedirect
+winget install -e --id Plex.PlexMediaPlayer
+winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+winget install -e --id Microsoft.PowerShell
+winget install -e --id JanDeDobbeleer.OhMyPosh
+winget install -e --id Brave.Brave
+winget install -e --id VideoLAN.VLC
+winget install -e --id Greenshot.Greenshot
+winget install -e --id voidtools.Everything
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Valve.Steam
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id CodecGuide.K-LiteCodecPack.Mega
+winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id Synology.DriveClient
+winget install -e --id Synology.ActiveBackupForBusinessAgent
+winget install -e --id qBittorrent.qBittorrent
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id Discord.Discord
+winget install -e --id ProtonTechnologies.ProtonVPN
+winget install -e --id 7zip.7zip
+winget install -e --id TeamViewer.TeamViewer
+winget install -e --id JAMSoftware.TreeSize.Free
+winget install -e --id WinMerge.WinMerge
+winget install -e --id Nilesoft.Shell
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id Neovim.Neovim
+winget install -e --id ElectronicArts.EADesktop
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id clsid2.mpc-hc
+#winget install -e --id Git.Git
+#winget install -e --id yt-dlp.yt-dlp
+#winget install -e --id Gyan.FFmpeg
+#winget install -e --id Microsoft.Office
+#winget install -e --id Microsoft.SQLServer.2019.Developer
+
+#Install-Module -Name Terminal-Icons -Repository PSGallery
+
+#https://www.partitionwizard.com/partitionmagic/powershell-windows-update.html
+#Install-Module -Name PSWindowsUpdate
+#Get-WindowsUpdate
+#Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+#Get-WUHistory 
+
+
+
+#Install-Module -Name RunAsUser
